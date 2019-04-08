c53aadb689858cd9757f31e8d39659c5b6a466ad
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-04-08 10:44:34 -0400

    Implements getAllListIDs and getListNameByID functions.

--- GLM/app/src/main/java/edu/qc/seclass/glm/DatabaseHelper.java
+++ GLM/app/src/main/java/edu/qc/seclass/glm/DatabaseHelper.java
@@ -1,8 +1,20 @@
 package edu.qc.seclass.glm;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author      Sean Rodriguez <sean.rodriguez@outlook.com>
+ * @version     1.0
+ * @since       1.0
+ */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
@@ -14,7 +26,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        // Present in future updates
+        // Future update: Implement data migration
     }
 
     @Override
@@ -22,10 +34,67 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(GroceryListContract.SQL_CREATE_ENTRIES);
 
         // Import default rows
-        // db.insert()
+        ContentValues values = new ContentValues();
+        values.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test");
+        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values);
     }
 
-    // TODO: Implement database helper functions
+    /**
+     * Returns all list IDs in GroceryList in a ArrayList
+     * @return ArrayList with all list IDs
+     */
+    public List getAllListIDs() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {
+                BaseColumns._ID,
+                GroceryListContract.GroceryList.COLUMN_NAME
+        };
+
+        Cursor cursor = db.query(
+                GroceryListContract.GroceryList.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(GroceryListContract.GroceryList._ID));
+            itemIds.add(itemId);
+        }
+
+        return itemIds;
+    }
+
+    public String getListNameByID(long ID) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] projection = {
+                BaseColumns._ID,
+                GroceryListContract.GroceryList.COLUMN_NAME
+        };
+
+        String selection = GroceryListContract.GroceryList._ID + " = ?";
+        String[] selectionArgs = { Long.toString(ID) };
+
+        Cursor cursor = db.query(
+                GroceryListContract.GroceryList.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+
+        cursor.moveToNext();
+        return cursor.getString(
+                cursor.getColumnIndexOrThrow(GroceryListContract.GroceryList.COLUMN_NAME));
+    }
+
     // Get all list names
     // Get items in list ID
     // Get item types

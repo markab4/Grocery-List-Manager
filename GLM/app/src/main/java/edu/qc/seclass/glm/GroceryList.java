9435692aa12bee0f37389c664aa5cff423a75acc
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-04-08 12:38:20 -0400

    Adds ID class member, adds documentation header

--- GLM/app/src/main/java/edu/qc/seclass/glm/GroceryList.java
+++ GLM/app/src/main/java/edu/qc/seclass/glm/GroceryList.java
@@ -1,12 +1,22 @@
 package edu.qc.seclass.glm;
 
+/**
+ * @author      Mark Abramov <markabramov01@gmail.com>
+ *              Sean Rodriguez <sean.rodriguez@outlook.com>
+ *
+ * @version     1.0
+ * @since       1.0
+ */
+
 public class GroceryList {
     private String listName;
     private boolean isSelected;
+    private long ID;
 
-    public GroceryList(String name){
-        listName = name;
-        isSelected = false;
+    public GroceryList(Long ID, String name){
+        this.ID = ID;
+        this.listName = name;
+        this.isSelected = false;
     }
 
     public String getListName() {

6661d2e784a0254f09ca77bb0298ea1a581a1662
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-04-10 21:44:12 -0400

    Removes test default rows

--- GLM/app/src/main/java/edu/qc/seclass/glm/DatabaseHelper.java
+++ GLM/app/src/main/java/edu/qc/seclass/glm/DatabaseHelper.java
@@ -36,31 +36,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(GroceryListContract.SQL_CREATE_ENTRIES);
-
-        // Import default rows
-        ContentValues values1 = new ContentValues();
-        values1.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test1");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values1);
-
-        ContentValues values2 = new ContentValues();
-        values2.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test2");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values2);
-
-        ContentValues values3 = new ContentValues();
-        values3.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test3");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values3);
-
-        ContentValues values4 = new ContentValues();
-        values4.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test4");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values4);
-
-        ContentValues values5 = new ContentValues();
-        values5.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test5");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values5);
-
-        ContentValues values6 = new ContentValues();
-        values6.put(GroceryListContract.GroceryList.COLUMN_NAME, "Test6");
-        db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values6);
     }
 
     /**
@@ -131,7 +106,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return db.insert(GroceryListContract.GroceryList.TABLE_NAME, null, values);
     }
 
-    // Get items in list ID
-    // Get item types
-    // Get items with item type ID
 }

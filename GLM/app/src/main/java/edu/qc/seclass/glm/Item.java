64f78488c394c01bba18acfd044fd12fc1dd0f9e
Sean Rodriguez <sean.rodriguez@outlook.com>
2019-04-11 20:22:12 -0400

    Adds class for Items

---
+++ GLM/app/src/main/java/edu/qc/seclass/glm/Item.java
@@ -0,0 +1,41 @@
+package edu.qc.seclass.glm;
+
+/**
+ * @author          Mark Abramov <markabramov01@gmail.com>
+ *                  Sean Rodriguez <sean.rodriguez@outlook.com>
+ *
+ * Class contains the id and name of the item, and the item type of the item
+ *
+ * @version     1.0
+ * @since       1.0
+ */
+
+public class Item {
+    private long ID;
+    private String name;
+    private ItemType type;
+
+    public Item(long ID, String name, ItemType type) {
+        this.ID = ID;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Item(long ID, String name, long typeID, String typeName) {
+        this.ID = ID;
+        this.name = name;
+        this.type = new ItemType(typeID, typeName);
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+}
